003e7e4d7b9bfc26889c5b533937c382aad864b9
JTarsa <jan.tarsa@protonmail.com>
2023-06-15 18:25:19 +0200

    func change

--- src/app/components/functionality-edit/functionality-edit.component.ts
+++ src/app/components/functionality-edit/functionality-edit.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FunctionalityInterface } from 'src/app/interfaces/functionality.interface';
-import { FunctionalityService } from 'src/app/services/functionality.service';
-import { TaskInterface } from 'src/app/interfaces/task.interface';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FunctionalityInterface } from "src/app/interfaces/functionality.interface";
+import { FunctionalityService } from "src/app/services/functionality.service";
+import { TaskInterface } from "src/app/interfaces/task.interface";
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
-} from '@angular/forms';
-import { WorkStatus } from 'src/app/enums/workStatus.enum';
-import { Priority } from 'src/app/enums/priority.enum';
+} from "@angular/forms";
+import { WorkStatus } from "src/app/enums/workStatus.enum";
+import { Priority } from "src/app/enums/priority.enum";
 
 @Component({
-  selector: 'app-functionality-edit',
-  templateUrl: './functionality-edit.component.html',
-  styleUrls: ['./functionality-edit.component.css'],
+  selector: "app-functionality-edit",
+  templateUrl: "./functionality-edit.component.html",
+  styleUrls: ["./functionality-edit.component.css"],
 })
 export class FunctionalityEditComponent implements OnInit {
   functionality: FunctionalityInterface | undefined;
@@ -23,6 +23,7 @@ export class FunctionalityEditComponent implements OnInit {
   workStatusValues = Object.values(WorkStatus);
   priorityValues = Object.values(Priority);
   functionalityId!: string;
+  functionalityOptions!: FunctionalityInterface;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,39 +33,40 @@ export class FunctionalityEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    console.log(this.workStatusValues);
     this.route.paramMap.subscribe((params) => {
-      const id = params.get('id');
+      const id = params.get("id");
       if (id !== null) {
         this.functionalityId = id;
         this.loadFunctionality();
       } else {
-        this.functionalityId = '';
+        this.functionalityId = "";
       }
     });
 
-    const currentDate = new Date().toISOString().split('T')[0];
+    this.functionalitiesService
+      .getSingleFunctionality(this.functionalityId)
+      .subscribe((functionality: FunctionalityInterface) => {
+        this.functionalityOptions = functionality;
+      });
+    console.log(this.functionalityOptions.status);
+    const currentDate = new Date().toISOString().split("T")[0];
     const addedDateControl = new FormControl({
       value: currentDate,
       disabled: true,
     });
 
     this.functionalityForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-      priority: [Priority.Low, Validators.required],
-      status: [WorkStatus.Todo, Validators.required],
+      name: [this.functionalityOptions.name, Validators.required],
+      description: [this.functionalityOptions.description, Validators.required],
+      priority: [this.functionalityOptions.priority, Validators.required],
+      status: [this.functionalityOptions.status, Validators.required],
       addedDate: addedDateControl,
-      startDate: [''],
-      endDate: [''],
-      timeSpent: [''],
+      startDate: [this.functionalityOptions.startDate],
+      endDate: [this.functionalityOptions.endDate],
+      timeSpent: [this.functionalityOptions.timeSpent],
     });
   }
-
-  // List task, ktore naleza do tej funkcjonalnosci
-  // 2. wszystkie funkcjonalnosci
-  // 3. id funkcjonalnosci do ktorej przynaleza te taski w liscie
-  // 4. dla kazdego obiekut z taskow sprawdz czy ktorys status jest doing
-  // 5. jezeli jest to funkctionality.status = doingol
 
   loadFunctionality() {
     this.functionalitiesService
@@ -81,9 +83,9 @@ export class FunctionalityEditComponent implements OnInit {
 
   updateFunctionalityStatus(taskStatus: string) {
     if (this.functionality) {
-      if (taskStatus === 'doing') {
+      if (taskStatus === "doing") {
         this.functionality.status = WorkStatus.Doing;
-      } else if (taskStatus === 'done') {
+      } else if (taskStatus === "done") {
         this.functionality.status = WorkStatus.Done;
       } else {
         this.functionality.status = WorkStatus.Todo;
@@ -94,6 +96,8 @@ export class FunctionalityEditComponent implements OnInit {
   editFunctionality() {
     const taskStatus = this.functionalityForm.value.status;
     this.updateFunctionalityStatus(taskStatus);
+
+    console.log(this.functionalityForm.value.status);
 
     if (this.functionality) {
       this.functionality.name = this.functionalityForm.value.name;
@@ -117,8 +121,7 @@ export class FunctionalityEditComponent implements OnInit {
         .updateFunctionality(this.functionality)
         .subscribe(
           (functionality) => {
-            console.log(functionality);
-            this.router.navigate(['/functionalities']);
+            this.router.navigate(["/functionalities"]);
           },
           (error) => {
             console.log(error);
